a2226a89ea2a392d520aec79bfdabd49e962a142
NDevTK <31563761+NDevTK@users.noreply.github.com>
2019-12-07 12:16:43 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-client.login("N/A");
+client.login(process.env.discord);
 
 const EmojiWhitelist = /[^\s|\n|\u200b|\u180B-\u180D\uFE00-\uFE0F|\uDB40[\uDD00-\uDDEF|\u00a9|\u00ae|\u2000-\u3300|\ud83c\ud000-\udfff|\ud83d\ud000-\udfff|\ud83e\ud000-\udfff]/g;
 

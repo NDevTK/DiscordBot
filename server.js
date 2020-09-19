98434cf49b30ee0c359f4b5d78debde117c5da69
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-09-20 00:46:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ client.login(process.env.discord);
 client.on("ready", () => {
 	client.user.setActivity("Fake News", { type: "WATCHING"});
 	anonymous = client.channels.get('656126149381849089');
+	bin2text = client.channels.get('757012300849872946');
 })
 
 const EmojiWhitelist = /[^\s|\n|\u{200b}|\u{180B}-\u{180D}\u{FE00}-\u{FE0F}|\u{DB40}|\u{DD00}-\u{DDEF}|\u{1f300}-\u{1f5ff}\u{1f900}-\u{1f9ff}\u{1f600}-\u{1f64f}\u{1f680}-\u{1f6ff}\u{2600}-\u{26ff}\u{2700}-\u{27bf}\u{1f1e6}-\u{1f1ff}\u{1f191}-\u{1f251}\u{1f004}\u{1f0cf}\u{1f170}-\u{1f171}\u{1f17e}-\u{1f17f}\u{1f18e}\u{3030}\u{2b50}\u{2b55}\u{2934}-\u{2935}\u{2b05}-\u{2b07}\u{2b1b}-\u{2b1c}\u{3297}\u{3299}\u{303d}\u{00a9}\u{00ae}\u{2122}\u{23f3}\u{24c2}\u{23e9}-\u{23ef}\u{25b6}\u{23f8}-\u{23fa}]/u
@@ -70,6 +71,10 @@ client.on("message", (message) => {
 		    message.delete();
             sendMessage(process.env.rot47, salad.ROT47.Cipher().crypt(message.content), message.author.username);
 			break;
+		case "757012300849872946": // Binary to Text
+		    message.delete();
+            bin2text.send(Bin2Text(message.content));
+			break;
 		case "653320664274436140": // Text to Binary
 		    message.delete();
             sendMessage(process.env.binary, Text2Bin(message.content), message.author.username);
@@ -85,7 +90,11 @@ client.on("message", (message) => {
 });
 
 function Text2Bin(input) {
-	return Array.from(input).map((each)=>each.codePointAt(0).toString(2)).join(" ");
+	return Array.from(input).map((each) => each.codePointAt(0).toString(2)).join(" ");
+}
+
+function Bin2Text(input) {
+	return input.split(' ').map((bin) => String.fromCharCode(parseInt(bin, 2))).join('');
 }
 
 function Text2Seed(input) {
